166a5bc110797fe3401f7edda8cfc039cdb77b93
ehhahappy <mqmqmqmqmq@naver.com>
2020-12-30 17:55:33 +0900

    Use mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://ehhahappy:gk266252@boilerplate.unhra.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connexted...')).catch(err => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
